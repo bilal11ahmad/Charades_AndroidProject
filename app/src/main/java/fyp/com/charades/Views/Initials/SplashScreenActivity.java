1c29e1434de93f5e384df5367352d8fab7424cd7
bilal11ahmad <bilal11.official@gmail.com>
2020-05-14 05:30:51 +0500

    first commit

---
+++ app/src/main/java/fyp/com/charades/Views/Initials/SplashScreenActivity.java
@@ -0,0 +1,229 @@
+package fyp.com.charades.Views.Initials;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import fyp.com.charades.R;
+import fyp.com.charades.Views.Dashboard.DashboardPage;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final String FILE_NAME ="Charades";
+    private static final String USER_LOGGED_KEY ="USER_LOGGED";
+    private static int SPLASH_SCREEN_DELAY=2500;
+
+    String[] appPermissions = new String[]{
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    private static final int PERMISSIONS_REQUEST_CODE = 1240;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        if (checkAndRequestPermissions())
+        {
+            ChangeActivity();
+
+        }
+    }
+
+    private void ChangeActivity(){
+        SharedPreferences preferences =  getSharedPreferences(FILE_NAME,MODE_PRIVATE);
+        int logged = preferences.getInt(USER_LOGGED_KEY,0);
+        if(logged==1){
+            MoveToDashboard();
+        }else{
+            MoveToMainScreen();
+        }
+    }
+
+    private void MoveToDashboard(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreenActivity.this, DashboardPage.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_DELAY);
+    }
+
+
+    private void MoveToMainScreen(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_DELAY);
+    }
+
+
+    public boolean checkAndRequestPermissions()
+    {
+        // Check which permissions are granted
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String perm : appPermissions)
+        {
+            if (ContextCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED)
+            {
+                listPermissionsNeeded.add(perm);
+            }
+        }
+
+        // Ask for non-granted permissions
+        if (!listPermissionsNeeded.isEmpty())
+        {
+            ActivityCompat.requestPermissions(this,
+                    listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),
+                    PERMISSIONS_REQUEST_CODE
+            );
+            return false;
+        }
+
+        // App has all permissions. Proceed ahead
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        if (requestCode == PERMISSIONS_REQUEST_CODE)
+        {
+            HashMap<String, Integer> permissionResults = new HashMap<>();
+            int deniedCount = 0;
+
+            // Gather permission grant results
+            for (int i=0; i<grantResults.length; i++)
+            {
+                // Add only permissions which are denied
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED)
+                {
+                    permissionResults.put(permissions[i], grantResults[i]);
+                    deniedCount++;
+                }
+            }
+
+            // Check if all permissions are granted
+            if (deniedCount == 0)
+            {
+                // Proceed ahead with the app
+                MoveToMainScreen();
+            }
+            // Atleast one or all permissions are denied
+            else
+            {
+                for (Map.Entry<String, Integer> entry : permissionResults.entrySet())
+                {
+                    String permName = entry.getKey();
+                    int permResult = entry.getValue();
+
+                    // permission is denied (this is the first time, when "never ask again" is not checked)
+                    // so ask again explaining the usage of permission
+                    // shouldShowRequestPermissionRationale will return true
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, permName))
+                    {
+                        // Show dialog of explanation
+                        showDialog("Permission Request.", "This app needs Location,Storage and Camera permissions to work wihout any issues and problems.\n" +
+                                        "Location is Required to show products from nearby Pharmacies.\n" +
+                                        "Camera and Storage is required to upload images of medicine presciption from doctor to place an order.\n",
+                                "Yes, Grant permissions",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.dismiss();
+                                        checkAndRequestPermissions();
+                                        dialogInterface.dismiss();
+                                    }
+                                },
+                                "No, Exit app", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.dismiss();
+                                        finish();
+                                    }
+                                }, false);
+                    }
+                    //permission is denied (and never ask again is  checked)
+                    //shouldShowRequestPermissionRationale will return false
+                    else
+                    {
+                        // Ask user to go to settings and manually allow permissions
+                        showDialog("", "You have denied some permissions to the app. Please allow all permissions at [Setting] > [Permissions] screen",
+                                "Go to Settings",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.dismiss();
+                                        // Go to app settings
+                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                                Uri.fromParts("package", getPackageName(), null));
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                },
+                                "No, Exit app", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.dismiss();
+                                        finish();
+                                    }
+                                }, false);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+    }
+
+
+    public AlertDialog showDialog(String title, String msg, String positiveLabel,
+                                  DialogInterface.OnClickListener positiveOnClick,
+                                  String negativeLabel, DialogInterface.OnClickListener negativeOnClick,
+                                  boolean isCancelAble)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setCancelable(isCancelAble);
+        builder.setMessage(msg);
+        builder.setPositiveButton(positiveLabel, positiveOnClick);
+        builder.setNegativeButton(negativeLabel, negativeOnClick);
+
+        AlertDialog alert = builder.create();
+        alert.show();
+        return alert;
+    }
+
+}
